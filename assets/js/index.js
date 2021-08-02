e1e5ccee67d2823287307e31b0c0bcae67dbacff
José Monte <josecmontejr@gmail.com>
2021-08-02 01:59:46 -0300

    man | create random food | refactory snake passing sides screen | create stop game

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,7 +11,7 @@ const Canvas = {
         return this.canvasContext;
     },
 
-    renderCanvasShape(dimensions, bgColor = "lightgreen") {
+    renderCanvasShape(dimensions, bgColor) {
         const { x, y, width, height } = dimensions;
         this.getCanvasContext().fillStyle = bgColor;
         this.getCanvasContext().fillRect(x, y, width, height);
@@ -22,7 +22,9 @@ const Game = {
     screen: null,
     squareSize: 32,
     snake: [],
-    direction: 'ArrowRight',
+    food: {},
+    direction: "ArrowRight",
+    start: null,
 
     init(canvas) {
         this.screen = canvas;
@@ -31,9 +33,9 @@ const Game = {
             y: 8 * this.squareSize,
         });
 
-        let start = setInterval(
-            this.startGame.bind(this), 100
-        );
+        this.createFoodRandom();
+
+        this.start = setInterval(this.startGame.bind(this), 100);
     },
 
     createSnake() {
@@ -47,70 +49,135 @@ const Game = {
         });
     },
 
+    createFoodRandom() {
+        this.food = {
+            x: Math.floor(Math.random() * 15 + 1) * this.squareSize,
+            y: Math.floor(Math.random() * 15 + 1) * this.squareSize,
+        };
+    },
+
+    drawFood() {
+        this.screen.renderCanvasShape(
+            {
+                x: this.food.x,
+                y: this.food.y,
+                width: this.squareSize,
+                height: this.squareSize,
+            },
+            "brown"
+        );
+    },
+
     renderScreen() {
-        this.screen.renderCanvasShape({
-            x: 0,
-            y: 0,
-            width: 16 * this.squareSize,
-            height: 16 * this.squareSize,
-        });
+        this.screen.renderCanvasShape(
+            {
+                x: 0,
+                y: 0,
+                width: 16 * this.squareSize,
+                height: 16 * this.squareSize,
+            },
+            "lightgreen"
+        );
 
         this.createSnake();
+        this.drawFood();
     },
 
     passingSnake() {
-        if(this.snake[0].x > 15 * this.squareSize) {
-            this.snake[0].x = 0;
+        const biggerThanSize = (axis, size) => {
+            if (axis > 15 * size) {
+                return 0;
+            }
+            return axis;
         }
-        if(this.snake[0].x < 0) {
-            this.snake[0].x = 16 * this.squareSize;
+        const smallerThanZero = (axis, size) => {
+            if (axis < 0) {
+                return 16 * size;
+            }
+            return axis;
         }
-        if(this.snake[0].y > 15 * this.squareSize) {
-            this.snake[0].y = 0;
-        }
-        if(this.snake[0].y < 0) {
-            this.snake[0].y = 16 * this.squareSize;
-        }
-    },
-
-    handleKeyDown(event) {
-        console.log(`KeyDown Listener disptach ${event.code} pressed!`);
-        this.direction = event.code;
+        this.snake[0].x = biggerThanSize(this.snake[0].x, this.squareSize);
+        this.snake[0].y = biggerThanSize(this.snake[0].y, this.squareSize);
+        this.snake[0].x = smallerThanZero(this.snake[0].x, this.squareSize);
+        this.snake[0].y = smallerThanZero(this.snake[0].y, this.squareSize);
     },
 
     KeyboardEventListener() {
-        // let direction = this.direction;
-        document.addEventListener('keydown', this.handleKeyDown.bind(this));
+        const moveCommands = {
+            ArrowRight: "ArrowLeft",
+            ArrowLeft: "ArrowRight",
+            ArrowUp: "ArrowDown",
+            ArrowDown: "ArrowUp",
+        };
+
+        document.addEventListener(
+            "keydown",
+            function (event) {
+                if (
+                    moveCommands[event.code] &&
+                    moveCommands[this.direction] !== event.code
+                ) {
+                    this.direction = event.code;
+                }
+            }.bind(this)
+        );
+    },
+
+    stopGame(gameStart) {
+        if (this.snake.length > 1) {
+            for (let i = 1; i < this.snake.length; i++) {
+                if (
+                    this.snake[0].x === this.snake[i].x &&
+                    this.snake[0].y === this.snake[i].y
+                ) {
+                    clearInterval(gameStart);
+                    alert("Game Over :(");
+                }
+            }
+        }
+    },
+
+    incrementScore() {
+        let score = document.getElementById('score').innerText;
+        document.getElementById('score').innerText = parseInt(score) + 1;
     },
 
     startGame() {
         this.passingSnake();
         this.renderScreen();
 
+        this.stopGame(this.start);
         this.KeyboardEventListener();
 
         const snake = { ...this.snake[0] };
 
         const moveCommands = {
-            ArrowRight: function(snake, size) {
+            ArrowRight: function (snake, size) {
                 snake.x += size;
             },
-            ArrowLeft: function(snake, size) {
+            ArrowLeft: function (snake, size) {
                 snake.x -= size;
             },
-            ArrowUp: function(snake, size) {
+            ArrowUp: function (snake, size) {
                 snake.y -= size;
             },
-            ArrowDown: function(snake, size) {
+            ArrowDown: function (snake, size) {
                 snake.y += size;
             },
         };
 
-        if(moveCommands[this.direction]) moveCommands[this.direction](snake, this.squareSize);
-        this.snake.pop();
+        if (moveCommands[this.direction])
+            moveCommands[this.direction](snake, this.squareSize);
+
+        if (snake.x !== this.food.x || snake.y !== this.food.y) {
+            this.snake.pop();
+        } else {
+            this.createFoodRandom();
+            this.incrementScore();
+        }
         const newHead = { ...snake };
         this.snake.unshift(newHead);
-    }
+    },
 };
 
 const canvasItem = Canvas;
